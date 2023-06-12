ff7507d0a8d433826cb03f9b04cda3fd6578939e
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-06-12 15:10:37 -0300

    cards e heading3 para revisar, pre finalizados

---
+++ src/components/cards/TeamMemberCard.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import '../../styles/cards/DefaultCard.css';
+import '../../styles/cards/TeamMemberCard.css';
+
+function TeamMemberCard({photo, name, dialogBtnText, dialogText}) {
+  const openModal = () => {
+    const dialog = document.getElementById(name)
+    dialog.showModal();
+  }
+  return (
+    <div className="card">
+      <img className="photo" src={photo} alt="foto do membro" />
+      <h4>{name}</h4>
+      <button className="openDialogBtn" onClick={ () => openModal() }>{dialogBtnText}</button>
+      <dialog id={name}>
+        <p>{dialogText}</p>
+      </dialog>
+    </div>
+  );
+}
+
+TeamMemberCard.propTypes = ({
+  photo: PropTypes.string,
+  name: PropTypes.string,
+}).isRequired;
+
+export default TeamMemberCard;
