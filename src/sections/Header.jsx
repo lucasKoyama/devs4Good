869eabc090dfd97edff1681f81ca67f545877e23
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-06-12 23:33:38 -0300

    falta só a responsividade

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -27,27 +27,3 @@ function Header() {
 }
 
 export default Header;
-
-
-// <header>
-//     <div class="container" id="nav-container">
-//       <nav class="navbar navbar-expand-lg fixed-top navbar-dark">
-//         <a class="navbar-brand" href="#about-area">
-//           <img id="logo" src="img/astronauta2.jpg" alt="hDC Agency"><h5 class="he">Devs<span class=" forgood at">4Good</span></h5>
-//         </a>
-//         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links" aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
-//           <span class="navbar-toggler-icon"></span>
-//         </button>
-//         <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
-//           <div class="navbar-nav">
-//             <a class="nav-item nav-link" id="home-menu" href="#">Home</span></a>
-//             <a class="nav-item nav-link" id="about-menu" href="#about-area">A empresa</a>
-//             <a class="nav-item nav-link" id="services-menu" href="#services-area">Serviços</a>
-//             <a class="nav-item nav-link" id="team-menu" href="#team-area">Time</a>
-//             <a class="nav-item nav-link" id="portfolio-menu" href="#portfolio-area">Projetos</a>
-//             <a class="nav-item nav-link" id="contact-menu" href="#contact-area">Contato</a>
-//           </div>
-//         </div>
-//       </nav>
-//     </div>
-//   </header>
--- src/sections/Projetos.jsx
+++ src/sections/Projetos.jsx
@@ -18,26 +18,3 @@ function Projetos() {
 }
 
 export default Projetos;
-
-// <div id="portfolio-area">
-//       <!-- <hr> -->
-//       <div class="container">
-//           <div class="row">
-//             <div class="col-md-12">
-//               <h3 class="main-title">Conheça nossos projetos</h3>
-//             </div>
-//             <div class="col-md-4 project-box dev">
-//               <img src="img/em-breve-glitch.gif" class="img-fluid" alt="Projeto 1">
-//             </div>
-//             <div class="col-md-4 project-box dsg">
-//               <img src="img/em-breve-glitch.gif" class="img-fluid" alt="Projeto 2">
-//             </div>
-//             <div class="col-md-4 project-box seo">
-//               <img src="img/em-breve-glitch.gif" class="img-fluid" alt="Projeto 3">
-//             </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </main>
